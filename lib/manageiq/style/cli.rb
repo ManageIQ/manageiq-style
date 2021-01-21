ecc64ae3d4f5223d89d313fb080f63b38f0cc953
Jason Frey <fryguy9@gmail.com>
2021-01-21 18:34:45 -0500

    Update the gemspec or Gemfile when installing

--- lib/manageiq/style/cli.rb
+++ lib/manageiq/style/cli.rb
@@ -35,6 +35,7 @@ module ManageIQ
         ensure_rubocop_local_yml_exists
         update_codeclimate_yml
         update_generator
+        update_gem_source
       end
 
       private
@@ -119,6 +120,105 @@ module ManageIQ
         data["version"] ||= "2"
 
         File.write(".codeclimate.yml", data.to_yaml.sub("---\n", ""))
+      end
+
+      def update_gem_source
+        if (gemspec = Dir.glob("*.gemspec").first)
+          update_gemspec(gemspec)
+        elsif File.exist?("Gemfile")
+          update_gemfile
+        end
+      end
+
+      def update_gemspec(gemspec)
+        contents = File.read(gemspec)
+        return if contents.include?("manageiq-style")
+
+        lines = contents.lines
+
+        obj = $1 if lines.detect { |l| l =~ /^\s+(\w+)\.name\s+=/ }
+        raise "couldn't find spec object name" if obj.nil?
+
+        new_line = "#{obj}.add_development_dependency \"manageiq-style\"\n"
+
+        dev_lines = lines.select { |l| l.include?("add_development_dependency") }
+        if dev_lines.any?
+          # Add manageiq-style and remove all direct rubocop references
+          dev_lines << new_line
+          dev_lines.reject! { |l| l.include?("rubocop") }
+
+          # Prepare the lines in a nice format
+          format_gem_source_lines!(dev_lines)
+          dev_lines.sort_by!(&:downcase)
+
+          # Remove the old dev lines and add the new ones
+          insert_at = lines.index { |l| l.include?("add_development_dependency") }
+          lines.delete_if { |l| l.include?("add_development_dependency") }
+          lines.insert(insert_at, dev_lines)
+        else
+          insert_at = -1
+          insert_at -= 1 until lines[insert_at].strip != "end"
+          lines.insert(insert_at, new_line)
+        end
+
+        File.write(gemspec, lines.join)
+      end
+
+      def update_gemfile
+        contents = File.read("Gemfile")
+        return if contents.include?("manageiq-style")
+
+        lines = contents.lines
+
+        new_line = "gem \"manageiq-style\", :require => false\n"
+
+        group_index = lines.index { |l| l.match?(/group\(?\s*:development/) }
+        if group_index
+          # Determine the group range
+          group_index += 1
+          group_end = group_index
+          group_end += 1 until lines[group_end].strip == "end"
+          group_range = (group_index..group_end - 1)
+
+          # Split lines into "gem" units including their preceding comments or blank lines
+          gem_units = lines[group_range].slice_after { |l| l.match?(/^\s*gem/) }.to_a
+
+          # Add manageiq-style and remove all direct rubocop references
+          gem_units << [new_line]
+          gem_units.reject! { |u| u.last.include?("rubocop") }
+
+          # Prepare the lines in a nice format
+          gem_lines = gem_units.map(&:last)
+          format_gem_source_lines!(gem_lines)
+          gem_units.each_with_index { |u, i| u[-1] = gem_lines[i] }
+          gem_units.sort_by! { |u| u.last.downcase }
+          gem_units.flatten!
+
+          # Remove the old group lines and add the new ones
+          lines.slice!(group_range)
+          lines.insert(group_index, gem_units)
+        else
+          lines << <<~RUBY
+
+            group :development do
+              #{new_line}
+            end
+          RUBY
+        end
+
+        File.write("Gemfile", lines.join)
+      end
+
+      def format_gem_source_lines!(lines)
+        indent = lines.first.match(/^\s+/).to_s
+
+        lines.map! { |l| l.strip.gsub(/[()]/, " ").split(" ", 3) }             # Split to [prefix, gem, versions]
+        lines.each { |parts| parts[1].tr!("'", "\"")}                          # Ensure double quoted gem name
+        max_width = lines.map { |parts| parts[1].size }.max                    # Determine the widest gem name
+        lines.each { |parts| parts[1] = parts[1].ljust(max_width, " ") }       # Apply the width
+        lines.map! { |parts| parts.join(" ").strip.insert(0, indent) << "\n" } # Back to strings
+
+        lines
       end
 
       def update_generator
